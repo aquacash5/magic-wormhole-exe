c65c6a0ffa2090074bd339a3acdd70585d18a571
Kyle Bloom <kylejbloom@gmail.com>
2021-02-22 08:47:36 +0000

    Remove default values

--- magic-wormhole.portable/tools/chocolateyinstall.ps1
+++ magic-wormhole.portable/tools/chocolateyinstall.ps1
@@ -1,20 +1,20 @@
-﻿  
 $ErrorActionPreference = 'Stop'
 $packageName = '$env:ChocolateyPackageName'
-$url = 'https://github.com/aquacash5/magic-wormhole-exe/releases/download/0.12.0/wormhole.exe'
-$checksum = 'E658BC2B865C81AAC87EAB76173CC7597470089CFC7DFE0CE492B31D77D7D457'
+$url = ''
+$checksum = ''
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "wormhole.exe"
 try {
   Get-ChocolateyWebFile -PackageName "$packageName" `
-                        -FileFullPath "$installFile" `
-                        -Url "$url" `
-                        -Checksum "$checksum" `
-                        -ChecksumType "$checksumType"
+    -FileFullPath "$installFile" `
+    -Url "$url" `
+    -Checksum "$checksum" `
+    -ChecksumType "$checksumType"
   # create an empty sidecar metadata file for closed-source shimgen.exe to prevent blank black window
   Set-Content -Path ("$installFile.gui") `
-              -Value $null
-} catch {
+    -Value $null
+}
+catch {
   throw $_.Exception
 }
