4e1e6f658daf8e0df4bb2947fb67b2336ffcbb64
Kyle Bloom <kylejbloom@gmail.com>
2021-04-05 09:12:08 +0100

    Fix issues from chocolatey maintainers

--- magic-wormhole.portable/tools/chocolateyinstall.ps1
+++ magic-wormhole.portable/tools/chocolateyinstall.ps1
@@ -5,13 +5,8 @@ $checksum = ''
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "wormhole.exe"
-try {
-  Get-ChocolateyWebFile -PackageName "$packageName" `
-    -FileFullPath "$installFile" `
-    -Url "$url" `
-    -Checksum "$checksum" `
-    -ChecksumType "$checksumType"
-}
-catch {
-  throw $_.Exception
-}
+Get-ChocolateyWebFile -PackageName "$packageName" `
+  -FileFullPath "$installFile" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType"
